30d657dac1f39a12335b60a3af8a2a5824fcd6b6
DustynotRusty <tejasrinivas.dnr@gmail.com>
2022-05-24 09:58:54 +0530

    h

--- Python Class/Tk_prog.py
+++ Python Class/Tk_prog.py
@@ -1,7 +1,9 @@
 from tkinter import *
+from tkinter import font
 root = Tk()
 root.title("My Main Window")
 root.geometry("1000x400")
 root.configure(bg='white')
+list_fonts = list(font.families())
 root.wm_iconbitmap("Python Class\ACBF_PC.ico")
 root.mainloop()
