b5fdf69bc04526ab292811444f04b4eb17dae5dc
DustynotRusty <tejasrinivas.dnr@gmail.com>
2022-05-24 10:27:39 +0530

    Final commit

--- Python Class/Tk_prog.py
+++ Python Class/Tk_prog.py
@@ -2,8 +2,10 @@ from tkinter import *
 from tkinter import font
 root = Tk()
 root.title("My Main Window")
-root.geometry("1000x400")
+root.geometry("1000x400+100+100")
 root.configure(bg='white')
-list_fonts = list(font.families())
+c = Canvas(root, bg='grey', height=500, width=600, cursor='pencil')
+id = c.create_oval(100, 500, 101, 300, width=1, fill='cyan')
+c.pack()
 root.wm_iconbitmap("Python Class\ACBF_PC.ico")
 root.mainloop()
