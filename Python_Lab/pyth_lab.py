4faf41caa0b9e40380660c80d10ab865fbd3baef
DustynotRusty <tejasrinivas.dnr@gmail.com>
2022-06-19 22:26:13 +0530

    New hackerrank progs

--- Python_Lab/pyth_lab.py
+++ Python_Lab/pyth_lab.py
@@ -1,17 +1,11 @@
-def addComplex(z1, z2):
-    return z1 + z2
-
-
-z1 = complex(2, 3)
-z2 = complex(1, 2)
-print("Addition is : ", addComplex(z1, z2))
-# function that returns a complex number after subtracting
-
-
-def subComplex(z1, z2):
-    return z1-z2
-
-
-z1 = complex(2, 3)
-z2 = complex(1, 2)
-print("Subtraction is : ", subComplex(z1, z2))
+t_c = int(input())
+for _ in range(t_c):
+    a, b, c, d = map(int, input().split())
+    count = 0
+    if b > a:
+        count += 1
+    if c > a:
+        count += 1
+    if d > a:
+        count += 1
+    print(count)
