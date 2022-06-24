6fa73f956f96b4f021be26a0d2351d6c2431dacd
DustynotRusty <tejasrinivas.dnr@gmail.com>
2022-06-24 05:42:52 +0530

    Progs

--- Python_Lab/pyth_lab.py
+++ Python_Lab/pyth_lab.py
@@ -1,11 +1,34 @@
-t_c = int(input())
-for _ in range(t_c):
-    a, b, c, d = map(int, input().split())
-    count = 0
-    if b > a:
-        count += 1
-    if c > a:
-        count += 1
-    if d > a:
-        count += 1
-    print(count)
+class Solution:
+    def repeatedNumber(self, A):
+        num = len(A)
+        lst = []
+        a = 0
+        b = 0
+        f = 0
+        xor1 = A[0]
+        for i in range(1, num):
+            xor1 = xor1 ^ A[i]
+        for i in range(1, num + 1):
+            xor1 = xor1 ^ i
+        bit = xor1 & ~(xor1 - 1)
+        for i in range(num):
+            if (A[i] & bit) != 0:
+                a = a ^ A[i]
+            else:
+                b = b ^ A[i]
+        for i in range(1, num + 1):
+            if (i & bit) != 0:
+                a = a ^ i
+            else:
+                b = b ^ i
+        for i in range(num):
+            if a == A[i]:
+                f = 1
+                break
+        if f == 0:
+            lst.append(b)
+            lst.append(a)
+        else:
+            lst.append(a)
+            lst.append(b)
+        return lst
